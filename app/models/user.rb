65065c24ae80907bf203cc973eb33da117a1ade1
kurumi <krkrteikoku@gmail.comi>
2020-08-02 01:08:38 +0900

    CRUD機能、ログイン機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true
+  before_validation { email.downcase! }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+  has_many :blogs
+  has_many :favorites, dependent: :destroy
+end
