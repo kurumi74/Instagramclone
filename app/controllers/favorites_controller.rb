2a2b184b25da1b5e7c8b4168c38721abb3c31588
kurumi <krkrteikoku@gmail.comi>
2020-08-03 19:27:40 +0900

    お気に入り一覧機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -8,4 +8,8 @@ class FavoritesController < ApplicationController
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
   end
+
+  def index
+    @favorites = current_user.favorites
+  end
 end
